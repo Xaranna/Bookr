baee9bfacf4ea6b9d5f99144f6b3f06c351c5ecd
vk <vk@yandex.ru>
2024-06-14 19:21:15 +0300

    Add book search

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from reviews import views
+from . import views
 
 app_name = 'reviews'
 urlpatterns = [
     path('books/', views.book_list, name = 'book_list'),
-    path('books/<int:book_id>/', views.book_detail, name = 'book_detail'),
+    path('books/<int:id>/', views.book_detail, name = 'book_detail'),
 
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Book
+from .models import Book, Contributor
 from .utils import average_rating
+from .forms import SearchForm
 
 
 def index(request):
     return render(request, 'base.html')
 
 def book_search(request):
-    search_text = request.GET.get('search', '')
-    return render(request, 'reviews/search_results.html', {'search_text': search_text})
+    search_text = request.GET.get("search", "")
+    form = SearchForm(request.GET)
+    books = set()
+
+    if form.is_valid() and form.cleaned_data["search"]:
+        search = form.cleaned_data["search"]
+        search_in = form.cleaned_data.get("search_in") or "title"
+        if search_in == "title":
+            books = Book.objects.filter(title__icontains=search)
+        else:
+            fname_contributors = Contributor.objects.filter(
+                first_names__icontains=search
+            )
+
+            for contributor in fname_contributors:
+                for book in contributor.book_set.all():
+                    books.add(book)
+
+            lname_contributors = Contributor.objects.filter(
+                last_names__icontains=search
+            )
+
+            for contributor in lname_contributors:
+                for book in contributor.book_set.all():
+                    books.add(book)
+
+    return render(
+        request,
+        "reviews/search_results.html",
+        {"form": form, "search_text": search_text, "books": books},
+    )
+
 
 def book_list(request):
     books = Book.objects.all()
@@ -30,8 +61,8 @@ def book_list(request):
 
     return render(request, 'reviews/book_list.html', context)
 
-def book_detail(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
+def book_detail(request, id):
+    book = get_object_or_404(Book, id=id)
     if reviews := book.review_set.all():
         book_rating = average_rating([review.rating for review in reviews])
         context = {
